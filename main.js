0cda0eb2730089aa1114581c613b6f5a72604329
ravi yadav <raviyadav00063@gmail.com>
2020-02-18 00:47:30 +0530

    Initial Commit

---
+++ main.js
@@ -0,0 +1,19 @@
+setInterval(setClock,1000);
+const hour=document.querySelector('[data-hour]')
+const minute=document.querySelector('[data-minute]')
+const second=document.querySelector('[data-second]')
+function setClock()
+{
+  const nowDate=new Date;
+  const sR=nowDate.getSeconds()/60;
+  const mR=(sR+nowDate.getMinutes())/60;
+  const hR=(mR+nowDate.getHours())/12;
+  setRotation(hour,hR);
+  setRotation(minute,mR);
+  setRotation(second,sR);
+}
+function setRotation(element,rR)
+{
+  element.style.setProperty('--r',(rR*360));
+}
+setClock();
